105f0b5c294e446b31842066b176e1f8d3997931
jay sharma <jay@cloudganga.com>
2018-09-30 16:17:50 +0530

    product category added

--- src/app/dashboard-module/product-category/product-category.component.ts
+++ src/app/dashboard-module/product-category/product-category.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
+
 
 @Component({
   selector: 'app-product-category',
@@ -6,10 +8,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-category.component.scss']
 })
 export class ProductCategoryComponent implements OnInit {
-
-  constructor() { }
+  orderForm: FormGroup;
+  items: FormArray;
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
-  }
+    // this.orderForm = this.formBuilder.group({})
+     
 
-}
+  }
+  createType(){
+    this.orderForm = this.fb.group({
+      type: '',
+      category: this.fb.array([  ])
+    })
+    console.log(this.orderForm);
+    
+  }
+  createItem(): FormGroup {
+    return this.fb.group({
+      name: '',
+      value: '',
+    });
+  }
+  addcategory(): void {
+
+    // <FormArray>this.orderForm.get('category').p ;
+    this.items=this.orderForm.get('category') as FormArray;
+     this.items.push(this.createItem());
+     console.log(this.items);
+     this.orderForm.controls.category=this.items;
+
+  }
+  deletecategory(index){
+    this.items=this.orderForm.get('category') as FormArray;
+    this.items.removeAt(index);
+    
+  }
+    // this.orderForm.controls.category=this.formBuilder.array([this.createItem()])
+    // this.items = this.orderForm.get('category') as FormArray;
+    // this.items.push(this.createItem());
+    onSubmit(){
+    console.log("sas")
+    }
+  }
+ 
+ 
+
+
